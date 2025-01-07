4a72a6ace14afd1772269a2fe962d7e1b1714ddf
Maxime Vernay <vernaymaxime4@gmail.com>
2025-01-07 22:31:18 +0100

    ajout click dessin

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const drawButton = document.getElementById("drawButton");
 const canvas = document.getElementById("canvas");
 const colorPanel = document.getElementById("colorpanel");
 const widthselector = document.getElementById("lineWidth");
-
+const butonTelecharger = document.getElementById("telechargerImage");
 let liste_point = []
 let ctx = canvas.getContext("2d");
 ctx.lineWidth = 125;
 ctx.lineCap = "round";
-
 ctx.strokeStyle = '#ff0000';
+
+
+
+const telecharger  = (e) =>{
+    canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    window.location.href = canvas;
+}
+
 /**
  * @param {} e 
  */
@@ -18,6 +25,7 @@ ctx.strokeStyle = '#ff0000';
 
 
 function fillgap(){
+    
     ctx.beginPath();
     ctx.moveTo(liste_point[0][0],liste_point[0][1])
     ctx.lineTo(liste_point[1][0],liste_point[1][1]);
@@ -25,6 +33,17 @@ function fillgap(){
     liste_point.shift()
 
 }
+
+function dot(e){
+    ctx.beginPath();
+    ctx.moveTo(e.clientX-10, e.clientY);
+    ctx.lineTo(e.clientX-10, e.clientY);
+    ctx.stroke();
+    liste_point.shift()
+
+}
+
+
 
 const updatecolor = (e) =>{
     console.log(e.target.value);
@@ -41,12 +60,14 @@ const movemouse  = (e) => {
     if (liste_point.length > 2){
         fillgap()
     }
+    
 }
 
 drawButton.addEventListener("click" , function(){
     
     canvas.addEventListener("pointerdown",
         (event) => {
+            dot(event)
             addEventListener("mousemove", movemouse);
             canvas.addEventListener("mouseup",(event) => {
                 removeEventListener("mousemove",movemouse)
@@ -58,3 +79,4 @@ drawButton.addEventListener("click" , function(){
 
 colorPanel.addEventListener("change", updatecolor);
 widthselector.addEventListener("change", updatewidth);
+butonTelecharger.addEventListener("click", telecharger)
